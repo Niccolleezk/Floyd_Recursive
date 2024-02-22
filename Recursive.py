e74608d1412983fb0cd93428cc4c6b245a3ad59d
Niccolleezk <160758411+Niccolleezk@users.noreply.github.com>
2024-02-22 13:00:21 +0000

    Version 3_Unit Test

--- Recursive.py
+++ Recursive.py
@@ -5,10 +5,14 @@ def floyd (graph):
         if a == 0:
             return dist[b][c]
         else: 
-            return min(floyd_recursive(dist, a-1, b, c), floyd_recursive(dist, a-1, b, a) + floyd_recursive(dist, k-1, a, c))
+            return min(floyd_recursive(dist, a-1, b, c), floyd_recursive(dist, a-1, b, a) + floyd_recursive(dist, a-1, a, c))
     for a in range (n):
         for b in range (n):
             for c in range (n):
                 graph [b][c]= floyd_recursive (graph, a, b, c)
     return graph 
-        
+
+import unittest
+
+class TestingFloyd (unittest.TestCase):
+    def testing_floyd (self):
