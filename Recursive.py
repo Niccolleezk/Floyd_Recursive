ad62fca41a1e0acbfd9f8e7139ffc1c241708fb0
Niccolleezk <160758411+Niccolleezk@users.noreply.github.com>
2024-02-22 14:08:20 +0000

    Version 5_Imperative Version

--- Recursive.py
+++ Recursive.py
@@ -33,3 +33,28 @@ class TestingFloyd (unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+"Below is the imperative version of Floyd's Algorithim which is provided" 
+
+import sys
+import itertools
+graph = [
+            [0, 5, float('inf'), 10],
+            [float('inf'), 0, 3, float('inf')],
+            [float('inf'), float('inf'), 0, 1],
+            [float('inf'), float('inf'), float('inf'), 0]
+        ]
+MAX_LENGTH = len(graph[0])
+
+def floyd_iterative(distance):
+    for intermediate, start_node,end_node\
+    in itertools.product\
+    (range(MAX_LENGTH),range(MAX_LENGTH), range(MAX_LENGTH)):
+        if start_node == end_node:
+            distance[start_node][end_node] = 0
+        continue
+    distance[start_node][end_node] = min(distance[start_node][end_node], distance[start_node][intermediate] + distance[intermediate][end_node] )
+    print (distance)
+floyd (graph)
+
